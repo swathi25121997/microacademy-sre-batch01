aa5af0c54c8b06c81c4f639bb6d6b479114f13ad
swathi25121997 <104359251+swathi25121997@users.noreply.github.com>
2022-04-29 13:01:20 +0530

    adding my first script

---
+++ math calculation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo"enter the value of a"
+read $a
+echo"enter the value of b"
+read $b
+ add = $($a + $b)
+ sub = $($a + $b)
+ mul = $($a + $b)
+ div = $($a + $b)
+ 
+ echo "the result for add is $add"
+ echo "the result for sub is $sub"
+ echo "the result for mul is $mul"
+ echo "the result for div is $div"
